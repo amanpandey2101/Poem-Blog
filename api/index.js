c7407b2e8a8868fd509618b885d8f135c89cb7ac
amanpandey2101 <paman7681@gmail.com>
2024-12-09 02:05:50 +0530

    update

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -22,7 +23,7 @@ mongoose
 const __dirname = path.resolve();
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 
