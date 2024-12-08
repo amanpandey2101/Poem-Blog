9674043ebb8114d5be9b539267f7eef7e747a277
amanpandey2101 <paman7681@gmail.com>
2024-12-09 01:54:36 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://poem-verse.onrender.com',
-        secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://poem-verse.onrender.com',
+  //       secure: false,
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   plugins: [react()],
 });
